3ef9e85b1e88a0ee94f2c761c5a10faf67d66230
Paolo Gatti (pgi) <pgi@odoo.com>
2026-06-04 23:18:01 +0200

    [IMP] small adjustments to dates and comments

--- .screenlayout/office.sh
+++ .screenlayout/office.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-1 --off
 xrandr --output eDP --off --output HDMI-A-0 --off --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 
